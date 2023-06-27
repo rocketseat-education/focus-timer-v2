29649ee8f32eacea2e596e4b8af36629c4cc4197
Ana Silveira <ana.ccord@gmail.com>
2023-06-27 15:02:47 -0300

    atualizando o display do contador

---
+++ src/FocusTimer/timer.js
@@ -0,0 +1,11 @@
+import state from './state.js'
+import * as el from './elements.js'
+
+export function updateDisplay(minutes, seconds) {
+    minutes = minutes ?? state.minutes
+    seconds = seconds ?? state.seconds
+
+    el.minutes.textContent = String(minutes).padStart(2, "0")
+    el.seconds.textContent = String(seconds).padStart(2, "0")
+
+}
