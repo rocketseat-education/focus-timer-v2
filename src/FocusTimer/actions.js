eeeeb0c72109c7d1dcd1482e25fe143a97d5c2f7
Ana Silveira <ana.ccord@gmail.com>
2023-06-27 14:52:04 -0300

    alterando o estado da aplicação conforme a ação

--- src/FocusTimer/actions.js
+++ src/FocusTimer/actions.js
@@ -1,3 +1,21 @@
-export function toggleRunning(){
-    console.log('toggle running')
+import state from './state.js'
+
+
+export function toggleRunning() {
+    state.isRunning = document.documentElement.classList.toggle('running')
+}
+
+export function reset() {
+    state.isRunning = false
+    document.documentElement.classList.remove('running')
+    
+}
+
+export function set() {
+    
+}
+
+export function toggleMusic() {
+    state.isMute = document.documentElement.classList.toggle('music-on')
+    
 }
